9694ae0fec7e70fd9e6f9eedbe1a934f8fd88689
Valter2026 <dvalter06@gmail.com>
2026-09-19 21:52:25 -0300

    Atualização Idiomas

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ function updateProfileInfo(profileData) {
 }
 
 
-function  upadateSoftSkills(profileData) {
+function  updateSoftSkills(profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills');
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
 }
@@ -36,10 +36,16 @@ function updateHardSkills(profileData) {
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li> <img src="${skill.logo}" alt=" ${skill.name}" title="${skill.name}"></li>`).join('');
 }
 
+function updateLanguages(profileData) {
+    const languages = document.getElementById('languages');
+    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('');
+}
+
 (async () => {
     const profileData = await fetchProfileData();
     updateProfileInfo(profileData)
-    upadateSoftSkills(profileData)
+    updateSoftSkills(profileData)
     updateHardSkills(profileData)
+    updateLanguages(profileData)
     
 })();
