4d65fa862094b40ffc9b86cf3a9eee8d673e11b4
Valter2026 <dvalter06@gmail.com>
2026-09-19 21:37:16 -0300

    Atualização hardskills e tools

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,11 +31,15 @@ function  upadateSoftSkills(profileData) {
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
 }
 
-
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills');
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li> <img src="${skill.logo}" alt=" ${skill.name}" title="${skill.name}"></li>`).join('');
+}
 
 (async () => {
     const profileData = await fetchProfileData();
     updateProfileInfo(profileData)
     upadateSoftSkills(profileData)
+    updateHardSkills(profileData)
     
 })();
